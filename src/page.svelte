043703befb779b1a06cd5337055e460a525dcb00
satohirrrrrr <sato1208hiro@gmail.com>
2021-06-05 11:01:44 +0900

    wip page

---
+++ src/page.svelte
@@ -0,0 +1,25 @@
+<div class="container__header">
+  <div class="d-flex">
+    <div class="header__add-card-btn">
+      <button type="button">+追加</button>
+    </div>
+    <div class="header__total-label">
+      <span>総額：</span>
+    </div>
+    <div class="header__average-label">
+      <span>平均：</span>
+    </div>
+    <div class="header__save-btn">
+      <button type="button">Save</button>
+    </div>
+  </div>
+</div>
+<div class="container__content">
+  container__content
+</div>
+
+<style lang="scss">
+.d-flex {
+  display: flex;
+}
+</style>
